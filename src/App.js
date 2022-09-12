a276ac25282bb79556d0bd6528a7b8b4e9e976a5
A-Danil <dan190598@gmail.com>
2022-09-12 20:08:22 +0300

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import AnimatedNumber from "animated-number-react";
+
 
 function App() {
+
+  const [count, setCount] = useState(0);
+  const [plus, setPlus] = useState('');
+  const [minus, setMinus] = useState('');
+
+  function counting(e){
+    if(e.key === 'ArrowRight'){
+      setCount(count + 1)
+      setPlus('')
+      setMinus('')
+      setPlus('+1')
+      setTimeout(()=>{
+        setPlus('')
+      }, 1000)
+    } else if(e.key === "ArrowLeft"){
+      setCount(count - 1)
+      setPlus('')
+      setMinus('')
+      setMinus('-1')
+      setTimeout(()=>{
+        setMinus('')
+      }, 1000)
+    }
+  }
+
+
+  useEffect(()=>{
+    window.addEventListener('keydown', counting);
+    return () => {
+      window.removeEventListener('keydown', counting);
+    }
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="counter">
+        <AnimatedNumber value={count} formatValue={n=>n.toFixed(0)} className='h1' duration={500} />
+        {/* <h1><span>{minus}</span>{count}<span>{plus}</span></h1> */}
+        <span className="minus">{minus}</span>
+        <span className="plus">{plus}</span>
+      </div>
     </div>
   );
 }
