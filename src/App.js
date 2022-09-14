7dce86e60a75735018da1969f8c3f43a237c4a77
A-Danil <dan190598@gmail.com>
2022-09-14 23:40:37 +0300

    add sound and new design

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import { useEffect, useState } from "react";
-import AnimatedNumber from "animated-number-react";
+import { useEffect, useState, useRef } from "react";
+// import AnimatedNumber from "animated-number-react";
+import plusSound from './assets/audio/plus.mp3';
+import minusSound from './assets/audio/minus.mp3';
+import achived from './assets/audio/achived20.mp3';
 
 
 function App() {
@@ -7,27 +10,51 @@ function App() {
   const [count, setCount] = useState(0);
   const [plus, setPlus] = useState('');
   const [minus, setMinus] = useState('');
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [sound, setSound] = useState(plusSound);
 
-  function counting(e){
-    if(e.key === 'ArrowRight'){
-      setCount(count + 1)
-      setPlus('')
-      setMinus('')
-      setPlus('+1')
-      setTimeout(()=>{
-        setPlus('')
-      }, 1000)
-    } else if(e.key === "ArrowLeft"){
-      setCount(count - 1)
-      setPlus('')
-      setMinus('')
-      setMinus('-1')
-      setTimeout(()=>{
-        setMinus('')
-      }, 1000)
+  const audioElem = useRef(null);
+
+async function tooglePlay () {
+    if(!isPlaying){
+      await audioElem.current.load();
+      await audioElem.current.play();
+    } else {
+      await audioElem.current.pause();
     }
   }
 
+
+ function counting(e){
+    if(e.key === 'ArrowRight'){
+      setCount(count + 1);
+      setPlus('');
+      setMinus('');
+      if(count !==0 && (count + 1) % 20 === 0){
+        setSound(achived);
+      } else {
+        setSound(plusSound);
+      }
+      setIsPlaying(true);
+      tooglePlay();
+      setIsPlaying(false);
+      setPlus('+1');
+      setTimeout(()=>{
+        setPlus('');
+      }, 1000)
+    } else if(e.key === "ArrowLeft"){
+      setCount(count - 1);
+      setPlus('');
+      setSound(minusSound);
+      setIsPlaying(true);
+      tooglePlay();
+      setIsPlaying(false);
+      setMinus('-1');
+      setTimeout(()=>{
+        setMinus('');
+      }, 1000)
+    }
+  }
 
   useEffect(()=>{
     window.addEventListener('keydown', counting);
@@ -39,10 +66,10 @@ function App() {
   return (
     <div className="container">
       <div className="counter">
-        <AnimatedNumber value={count} formatValue={n=>n.toFixed(0)} className='h1' duration={500} />
-        {/* <h1><span>{minus}</span>{count}<span>{plus}</span></h1> */}
-        <span className="minus">{minus}</span>
-        <span className="plus">{plus}</span>
+        <h1>{count}</h1>
+        <audio ref={audioElem} src={sound} preload="auto" />
+        <h3 className="minus">{minus}</h3>
+        <h3 className="plus" style={{right: `${count === 100 ? '' : '50%'}`}}>{plus}</h3>
       </div>
     </div>
   );
